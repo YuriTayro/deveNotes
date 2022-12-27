a1d8275a4cd4e7128fa6c5c56ff61d641170416b
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2022-12-27 09:49:53 -0300

    Add files via upload
    Primeiro commit.

---
+++ api/getall.php
@@ -0,0 +1,26 @@
+<?php
+require('../config.php');
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+
+if( $method === 'get'){
+
+    $sql = $pdo->query("SELECT*FROM notes");
+    if($sql->rowCount()>0){
+        $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($data as $item){
+            $array['results'][] = [
+                'id' => $item['id'],
+                'title' => $item['title']
+            ];
+        }
+
+    }
+
+} else {
+    $array['error'] = 'Método não permitido (apenas GET)';
+}
+
+
+require('../return.php');
